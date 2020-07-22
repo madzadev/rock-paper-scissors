983f1b26489fa87452924c0257669a8345f54d67
madarsbiss <madars.biss@gmail.com>
2020-07-22 13:03:48 +0300

    minor updates

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
