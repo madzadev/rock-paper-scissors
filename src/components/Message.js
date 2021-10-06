a48041d9bd558990fa116f272de95889104ac96d
madarsbiss <madars.biss@gmail.com>
2021-10-06 10:50:53 +0300

    fix msg

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,7 +3,7 @@ import "./Message.css";
 export const Message = ({ userSelection, message }) => {
   return (
     <div className="message-box">
-      {userSelection === "" ? <h1>VS</h1> : <h2>{message}</h2>}
+     <h2>{userSelection === "" ? "VS" : message}</h2>
     </div>
   );
 };
